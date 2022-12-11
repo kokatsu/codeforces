214aadee3bb9535d09d0d5d001ee3ddead53706a
kokatsu <66732063+kokatsu@users.noreply.github.com>
2022-12-11 22:05:44 +0900

    solve problem1370a

---
+++ problemset/problem1351_1400/problem1370/problem1370a.d
@@ -0,0 +1,14 @@
+import std;
+
+void main() {
+    int t;
+    readf("%d\n", t);
+
+    foreach (_; 0 .. t) {
+        int n;
+        readf("%d\n", n);
+
+        int res = n / 2;
+        res.writeln;
+    }
+}
